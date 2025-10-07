48fa3fab862cee7cd049b92dd3d584464e758123
Ranjith Kumar <ranjithkumarranjithkumar@avantaifintech.com>
2025-10-07 19:09:04 +0530

    Add GitHub Actions workflow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello! CI/CD with Docker is working Ranjith!asddasdas hello sdfksdkfhsdhfbhdbfkhsbfhidsbf khsdbfkhdsbfhdsbf kumar");
+  res.send("Hello! CI/CD with Docker is working Ranjith!asddasdas hello sdfksdkfhsdhfbhdbfkhsbfhidsbf khsdbfkhdsbfhdsbf kumar test");
 });
 
 const PORT = process.env.PORT || 3000;
