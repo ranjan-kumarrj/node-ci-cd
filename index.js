b9add91df1a7d1d6fee88b9a69a78b45e3fa81ef
Ranjith Kumar <ranjithkumarranjithkumar@avantaifintech.com>
2025-10-08 10:46:00 +0530

    ci cd is running

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hellow world");
+  res.send("Hellow world hsd");
 });
 
 const PORT = process.env.PORT || 3000;
